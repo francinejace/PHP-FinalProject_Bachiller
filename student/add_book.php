21cce6936aa1a2e6154dc60961219d4e30f1dc6c
francinejace <202310726@fit.edu.ph>
2025-07-16 10:58:36 +0800

    Refactor CSS styles for navigation links; streamline hover effects by removing unnecessary properties and ensuring consistent appearance across the menu.

---
+++ student/add_book.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Book</title>
+</head>
+<body>
+    <h1>Add Book</h1>
+    <form action="" method="post">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required><br><br>
+        <label for="author">Author:</label>
+        <input type="text" id="author" name="author" required><br><br>
+        <input type="submit" value="Add Book">
+    </form>
+</body>
+</html> 
